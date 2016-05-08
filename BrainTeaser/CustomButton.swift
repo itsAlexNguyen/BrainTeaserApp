e7a804add3ef2fb71d58c4fad865ef2faa48e52e
Alex Nguyen <alexnguyen@Alexs-MacBook-Pro.local>
2016-05-08 19:23:47 -0400

    Added animated button using FaceBook POP

--- BrainTeaser/CustomButton.swift
+++ BrainTeaser/CustomButton.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import pop
+
 @IBDesignable
 class CustomButton: UIButton {
     @IBInspectable var cornerRadius: CGFloat = 3.0 {
@@ -28,5 +30,26 @@ class CustomButton: UIButton {
     }
     func setupView() {
         self.layer.cornerRadius = cornerRadius
+        self.addTarget(self, action: "scaleToSmall", forControlEvents: .TouchDown)
+        self.addTarget(self, action: "scaleToSmall", forControlEvents: .TouchDragEnter)
+        self.addTarget(self, action: "scaleAnimation", forControlEvents: .TouchUpInside)
+        self.addTarget(self, action: "scaleToDefault", forControlEvents: .TouchDragExit)
+    }
+    func scaleToSmall() {
+        let scaleAnimation = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)
+        scaleAnimation.toValue = NSValue(CGSize: CGSizeMake(0.95, 0.95))
+        self.layer.pop_addAnimation(scaleAnimation, forKey: "layerScaleSmallAnimation")
+    }
+    func scaleAnimation() {
+        let scaleAnimation = POPSpringAnimation(propertyNamed: kPOPLayerScaleXY)
+        scaleAnimation.velocity = NSValue(CGSize: CGSizeMake(3.0, 3.0))
+        scaleAnimation.toValue = NSValue(CGSize: CGSizeMake(1.0,1.0))
+        scaleAnimation.springBounciness = 18
+        self.layer.pop_addAnimation(scaleAnimation, forKey: "LayerScaleSpringAnimation")
+    }
+    func scaleDefault(){
+        let scaleAnimation = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)
+        scaleAnimation.toValue = NSValue(CGSize: CGSizeMake(1, 1))
+        self.layer.pop_addAnimation(scaleAnimation, forKey: "LayerScaleScaleDefault")
     }
 }
